1e29bdaee7fc780188be30cfcbb34637eae5ca93
Christopher Paxton <cpaxton3@jhu.edu>
2015-09-02 12:10:47 -0400

    modifications

--- src/filter.cpp
+++ src/filter.cpp
@@ -15,43 +15,24 @@ ros::Subscriber sub;
 
 using namespace pcl_conversions;
 
+// Create the filtering object
+pcl::PassThrough<pcl::PCLPointCloud2> pass;
+
 void callback(const sensor_msgs::PointCloud2 &pc) {
 
-  pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGBA>());
-  pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZRGBA>());
+  pcl::PCLPointCloud2::Ptr cloud(new pcl::PCLPointCloud2);;
+  pcl::PCLPointCloud2 cloud_filtered;
 
-  pcl::PCLPointCloud2 pcl_pc;
-  pcl_conversions::toPCL(pc, pcl_pc);
+  pcl_conversions::toPCL(pc, *cloud);
 
-  pcl::fromPCLPointCloud2(pcl_pc, *cloud);
-
-  if( cloud->empty() == true ) {
-    ROS_ERROR("Recieved empty point cloud message!");
-    return;
-  }
-
-  std::cerr << "Cloud before filtering: " << cloud->points.size() << std::endl;
-  //for (size_t i = 0; i < cloud->points.size (); ++i)
-  //  std::cerr << "    " << cloud->points[i].x << " " 
-  //    << cloud->points[i].y << " " 
-  //    << cloud->points[i].z << std::endl;
-
-  // Create the filtering object
-  pcl::PassThrough<pcl::PointXYZRGBA> pass;
   pass.setInputCloud (cloud);
   pass.setFilterFieldName ("z");
-  pass.setFilterLimits (0.0, 1.0);
+  pass.setFilterLimits (0.2, 2.0);
   //pass.setFilterLimitsNegative (true);
-  pass.filter (*cloud_filtered);
-
-  std::cerr << "Cloud after filtering: " << cloud_filtered->points.size() << std::endl;
-  //for (size_t i = 0; i < cloud_filtered->points.size (); ++i)
-  //  std::cerr << "    " << cloud_filtered->points[i].x << " " 
-  //    << cloud_filtered->points[i].y << " " 
-  //    << cloud_filtered->points[i].z << std::endl;
+  pass.filter (cloud_filtered);
 
   sensor_msgs::PointCloud2 output_msg;
-  pcl::toROSMsg(*cloud_filtered,output_msg);
+  pcl_conversions::moveFromPCL(cloud_filtered,output_msg);
   output_msg.header.frame_id = pc.header.frame_id;
   pub.publish(output_msg);
 }
